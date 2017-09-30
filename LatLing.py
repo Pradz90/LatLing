2754451b0cf8f832cd7fb7b00e811a7f40fa261e
Nils F. Schmidt <nils.schmidt@uni-duesseldorf.de>
2017-09-30 19:09:37 +0200

    LatLing hinzugefügt
    Hauptdatei zum Ausführen für "LatLing" zum Repository hinzugefügt

---
+++ LatLing.py
@@ -0,0 +1,11 @@
+# author:   Nils F. Schmidt
+# email:    nils.schmidt@uni-duesseldorf.de
+# created:  2017-09-30
+# note:		This file loads all necessary scripts to use LatLing. For the available functions please look at the readme-file.
+
+print("LatLing Version 1.0")
+print("===================")
+print("LatLing is optimised for Python 3.6")
+print("Loading scripts:")
+from Scripts.sequence_align import *
+print("'Sequence Align' loaded.")
